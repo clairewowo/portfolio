79f8b8d8c84165ac2113118995445b19ac9ab1b3
clairewowo <claire.ze.wang@gmail.com>
2025-01-31 14:23:44 -0800

    step 1 done

--- global.js
+++ global.js
@@ -87,3 +87,34 @@ select.addEventListener('input', function (event) {
   localStorage.setItem('colorScheme', newScheme); // Save the new scheme to localStorage
 });
 
+export async function fetchJSON(url) {
+  try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      console.log(response)
+      const data = await response.json();
+      return data; 
+
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+};
+
+export function renderProjects(project, containerElement, headingLevel='h2') {
+  containerElement.innerHTML = '';
+  let x = 0;
+  for (let p of project) {
+    const article = document.createElement('article');
+    article.innerHTML = `
+    <${headingLevel}>${p.title}</${headingLevel}>
+    <img src="${p.image}" alt="${p.title}">
+    <p>${p.description}</p>
+    `;
+    containerElement.appendChild(article);
+    x += 1;
+  }
+  document.querySelector("h1").innerText = `${x} Projects`;
+};
